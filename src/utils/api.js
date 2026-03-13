cad57277c754ba90f38a5ae41bf9eb340d64e329
manoj <manojkumar_devarapu@srmap.edu.in>
2026-03-13 23:57:03 +0530

    fix API baseURL for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+    ? `${process.env.NEXT_PUBLIC_API_URL}/api`
+    : '/api'
+});
 
 api.interceptors.request.use(config => {
   const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
